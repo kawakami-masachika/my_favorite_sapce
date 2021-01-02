c3b512dd03e00d6b17a6d2594fe97466b6374235
kawakami-masachika <supercell1023koeda@gmail.com>
2021-01-02 13:04:23 +0900

    Styleモデルの実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'CSV'
+
 # テストユーザーの作成
 EMAIL = 'test@example.com'
 PASSWORD = 'password'
@@ -6,3 +8,16 @@ User.find_or_create_by(email: EMAIL) do |user|
   user.password = PASSWORD
   puts "サンプルユーザー登録完了"
 end
+
+# CSV読み込みスタイルのオプション
+option = {headers: true}
+
+# 系統マスタのレコードを作成
+style_file_path = "db/csv/style.csv"
+style_list = []
+CSV.foreach(style_file_path, option) do |row|
+  style_list << row.to_h
+end
+
+Style.create!(style_list)
+puts "系統モデルのCSVデータが登録できました。"
