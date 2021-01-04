1945a8966b89b0ddd9e496c51421aea007f4ab1e
kawakami-masachika <supercell1023koeda@gmail.com>
2021-01-04 19:57:50 +0900

    ブランドモデルとの関連付け

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,4 +2,6 @@ class Shop < ApplicationRecord
   belongs_to :user
   has_many :shop_styles, dependent: :destroy
   has_many :styles, through: :shop_styles
+  has_many :shop_brands, dependent: :destroy
+  has_many :brands, through: :shop_brands
 end
