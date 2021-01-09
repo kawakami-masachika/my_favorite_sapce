df2aa6bfce35ecc3b7dbad8a2352d68c78a911a9
kawakami-masachika <supercell1023koeda@gmail.com>
2021-01-09 14:33:34 +0900

    ショップサムネイルモデルの関連付け

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,6 @@
 class Shop < ApplicationRecord
   belongs_to :user
+  has_many :shop_images
   has_many :shop_styles, dependent: :destroy
   has_many :styles, through: :shop_styles
   has_many :shop_brands, dependent: :destroy
