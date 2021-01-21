d72c28d7fcad4570d6666643c1fa6a3419221bc4
kawakami-masachika <supercell1023koeda@gmail.com>
2021-01-21 21:44:18 +0900

    取り扱いカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_140129) do
+ActiveRecord::Schema.define(version: 2021_01_21_120951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 2021_01_07_140129) do
     t.bigint "user_id", null: false, comment: "投稿ユーザーID"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "treatment", null: false, comment: "取り扱い"
     t.index ["user_id"], name: "index_shops_on_user_id"
   end
 
