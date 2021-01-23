3e2ed98a820209cfe941786aba333924f758d2bc
kawakami-masachika <supercell1023koeda@gmail.com>
2021-01-23 10:18:27 +0900

    外部キーとユニークバリデーションの追加

--- app/models/shop_style.rb
+++ app/models/shop_style.rb
@@ -1,4 +1,8 @@
 class ShopStyle < ApplicationRecord
   belongs_to :shop
   belongs_to :style
+
+  # バリデーション
+  validates :shop, presence: true
+  validates :style_id, uniqueness: {scope: :shop_id}
 end
