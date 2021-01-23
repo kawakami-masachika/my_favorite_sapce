7de93e42ae3d53e5da566fcdde8e0b1829ca72f5
kawakami-masachika <supercell1023koeda@gmail.com>
2021-01-23 10:20:29 +0900

    ショップの系統登録処理の設設定バリデーションを追加

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,12 +1,14 @@
 class Shop < ApplicationRecord
   belongs_to :user
   has_many :shop_images
-  has_many :shop_styles, dependent: :destroy
+  has_many :shop_styles, dependent: :destroy, validate: true, inverse_of: :shop
   has_many :styles, through: :shop_styles
   has_many :shop_brands, dependent: :destroy
   has_many :brands, through: :shop_brands
 
   enum treatment: {male: 0, female: 1, unisex: 2}
+
+  accepts_nested_attributes_for :shop_styles, allow_destroy: true
 
   # バリデーション
   validates :shop_name, presence: true
@@ -17,4 +19,8 @@ class Shop < ApplicationRecord
   validates :sales_info, length: {maximum: 50}, if: Proc.new{|shop|shop.sales_info.present?}
   validates :shop_info, presence: true
   validates :shop_info, length: {maximum: 500}, if: Proc.new{|shop|shop.shop_info.present?}
+  validates :style_ids, presence: {message: :not_select}
+
+  # 関連付け先バリデーション
+  validates_associated :shop_styles
 end
