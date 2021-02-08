1270e32c4c8251bddc43484e9a0157f8b4c74a04
kawakami-masachika <supercell1023koeda@gmail.com>
2021-02-09 01:03:21 +0900

    バリデーションの追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :shop
+
+  validates :title, :rate, :content, presence: true
+  validates :title, length: {maximum: 50}, if: Proc.new{|review|review.title.present?}
+  validates :content, length: {maximum: 1000}, if: Proc.new{|review|review.content.present?}
 end
